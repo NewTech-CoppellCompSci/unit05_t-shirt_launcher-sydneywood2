6ac162152b86564a5e4fe1fff8c46ba72a2850f5
sydneywood2 <swood@newtech.coppellisd.com>
2023-01-18 15:07:07 -0600

    Unit 5
    unit 5

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -19,20 +19,74 @@ public class Lab01 {
 	
 	public static void problem1() {
 	
+		Scanner inKey = new Scanner(System.in);
+
+		System.out.print("Enter a positive integer: ");
+
+		int number = inKey.nextInt();
+
+		System.out.print("Enter another positive integer: ");
+
+		int exponent = inKey.nextInt();
+
 		
+
+		System.out.println(number + "^" + exponent + " = " + Math.pow(number, exponent) + "\n" );
+
+		
+
+		
+
+		
+
 	}
+	
 	
 	
 	
 	public static void problem2() {
 		
-			
-	}
+		Scanner inKey = new Scanner(System.in);
+
+		System.out.print("Enter a positive integer: ");
+
+		int number = inKey.nextInt();
+
+		
+
+		System.out.println(" The square root of " + number + " is " + Math.sqrt(number) + "\n" );
+
+		
+
+				
+
+	}	
+	
 	
 	
 	
 	public static void problem3() {
 		
+		Scanner inKey = new Scanner(System.in);
+
+		System.out.print("Length of side A: ");
+
+		int sideA = inKey.nextInt();
+
+		System.out.print("Length of side B: ");
+
+		int sideB = inKey.nextInt();
+
+		
+
+		
+
+		System.out.println("Hypotenuse = " + Math.hypot(sideA, sideB));
+
+		
+
+		System.out.println("\n");
+
 		
 	}
 	
@@ -40,9 +94,40 @@ public class Lab01 {
 	
 	public static void problem4() {
 		
+		Scanner inKey = new Scanner(System.in);
+
+		int max = Integer.MIN_VALUE;
+
+		int min = Integer.MAX_VALUE;
+
+		int input = -1;
+
 		
+
+		while (input != 0) {
+
+			System.out.println("Enter an integer: ");
+
+			input = inKey.nextInt();
+
+			
+
+			max = Math.max(max, input);
+
+			min = Math.min(min, input);
+
+		}
+
+			
+
+			System.out.println("Max = " + max);
+
+			System.out.println("Min = " + min);
+
+			
 	}
 	
 	
 	
 }
+
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -16,18 +16,90 @@ public class Lab02 {
 
 	public static void problem1() {
 		
+		Scanner inKey = new Scanner(System.in);
+
+		System.out.print("Enter a positive integer: ");
+
+		int input = inKey.nextInt();
+
+		System.out.print("Enter another positive integer: ");
+
+		int input2 = inKey.nextInt();
+
 		
+
+		int max = Math.max(input, input2);
+
+		int min = Math.min(input, input2);
+
+		
+
+		for (int i = 0; i < 100; i++ ) {
+
+		System.out.print((int) (Math.random () * ((max - min + 1)) + min) + " ");
+
+		
+
+	
+
+		}
+
+		
+
+		System.out.println("\n");
 	}
 
 
 	public static void problem2() {
 		
+Scanner inKey = new Scanner(System.in);
+
 		
+
+		System.out.print("What is the radius of the cylinder: ");
+
+		double r = inKey.nextDouble();
+
+		System.out.print("What is the height of the cylinder: ");
+
+		double h = inKey.nextDouble();
+
+		double volume = Math.PI * (r * r) * h;
+
+		System.out.print("The volume of the cylinder is " + volume + ". \n");
+
+		
+
+		System.out.println("\n");
 	}
 
 
 	public static void problem3() {
 		
+Scanner inKey = new Scanner(System.in);
+
+		
+
+		System.out.print("What is X1: \n");
+
+		double xone = inKey.nextDouble();
+
+		System.out.print("What is Y1: \n");
+
+		double yone = inKey.nextDouble();
+
+		System.out.print("What is X2: \n");
+
+		double xtwo = inKey.nextDouble();
+
+		System.out.print("What is Y2: \n");
+
+		double ytwo = inKey.nextDouble();
+
+		
+
+		System.out.println("Distance between points = " + Math.sqrt(((ytwo-yone)*(ytwo-yone))+((xtwo-xone)*(xtwo-xone))) + ". \n");
+
 		
 	}
 
@@ -35,6 +107,30 @@ public class Lab02 {
 	
 	public static void problem4() {
 		
+		Scanner inKey = new Scanner(System.in);
+
+		System.out.print("What is the a-value: \n");
+
+		double aVal = inKey.nextDouble();
+
+		System.out.print("What is the b-value: \n");
+
+		double bVal = inKey.nextDouble();
+
+		System.out.print("What is the c-value: \n");
+
+		double cVal = inKey.nextDouble();
+
+		
+
+		System.out.println("\n");
+
+		
+
+		System.out.println("x1 = " + (((-bVal + Math.sqrt(((bVal*bVal))-((4*aVal*cVal))))/(2*aVal))) + ".");
+
+		System.out.println("x2 = " + (((-bVal - Math.sqrt(((bVal*bVal))-((4*aVal*cVal))))/(2*aVal))) + ". \n");
+
 		
 	}
 
